c7d740aca174ebbfc8396ee1adc47da174f12d54
guilherme pessoa <guilhermepessoa7331@gmail.com>
2023-03-10 13:42:12 -0300

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		 <meta charset="utf-8">
+   		 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Sua loja de Jogos - Personal Gamer</title>
+		<link rel="icon" href="imagens/favicon.png">
+
+		<!-- Bootstrap -->
+    	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    	<link href="resource/css/estilo.css" rel="stylesheet">
+	</head>
+
+	<body>
+
+		<!--header-->
+		<nav class="navbar navbar-fixed-top navbar-inverse" ></nav>
+
+
+
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+         <meta charset="utf-8">
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Sua loja de Jogos - Personal Gamer</title>
+        <link rel="icon" href="app-laravel/resources/img/favicon.png">
+
+        <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="resources/css/estilo.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-fixed-top navbar-inverse" >
+        <div class="container">
+            <!-- header -->
+            <div class="navbar-header">
+                
+              <!-- botão toggle --> 
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="barra-navegacao">
+                  <span class="sr-only">alternar navegação</span><!--leitores de tela-->
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button> 
+
+              <!--Logo do site-->
+              <a href="index.html" class="navbar-brand">
+                <span class="img-logo">Personal Gamer</span> 
+              </a>
+
+            </div>
+
+            <!-- barra de navegação -->
+            <div class="collapse navbar-collapse" id="barra-navegacao">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Fale Conosco</a></li>
+                    <li><a href="#">Perfil</a></li>
+                    <li><a href="#">Carrinho</a></li>
+                </ul>
+            </div>
+        </div> <!-- /container -->
+    </nav> <!-- /nav --> 
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+
+
+</html>
